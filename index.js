60f0cff96a7d87c5a6f5f537667679a8839eb9b6
Danila Nefedov <dn375@mail.ru>
2022-08-02 23:25:36 +0400

    project completed

--- index.js
+++ index.js
@@ -8,16 +8,36 @@ input.addEventListener('keypress', (event) => {
         let task = document.createElement('span');
         task.appendChild(document.createTextNode(input.value));
         task.classList.add('task');
+        task.addEventListener('dblclick', () => {
+            let text = task.innerHTML;
+
+            let edit = document.createElement('input');
+            edit.value = text;
+            li.appendChild(edit);
+
+            edit.addEventListener('keypress', (event) => {
+                if (event.key == 'Enter') {
+                    task.innerHTML = edit.value;
+                    edit.remove();
+                }
+            });
+        });
         li.appendChild(task);
 
         let mark = document.createElement('span');
         mark.innerHTML = 'Выполнено';
         mark.classList.add('mark');
+        mark.addEventListener('click', () => {
+            li.classList.add('done');
+        });
         li.appendChild(mark);
 
         let remove = document.createElement('span');
         remove.innerHTML = 'Очистить';
         remove.classList.add('remove');
+        remove.addEventListener('click', () => {
+            li.remove();
+        });
         li.appendChild(remove);
 
         list.appendChild(li);
